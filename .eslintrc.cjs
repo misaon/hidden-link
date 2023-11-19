323b0f3de602cb6c6a1bb15fe3ee0d75bc214e3d
Ondřej Misák <email@ondrejmisak.cz>
2023-11-19 21:03:54 +0100

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,5 +25,11 @@ module.exports = {
         'vue/multi-word-component-names': 'off',
       },
     },
+    {
+      files: ['*.cjs'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
   ],
 };
